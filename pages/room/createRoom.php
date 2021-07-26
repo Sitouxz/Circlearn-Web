65206428216a41395d4d73c033563fa3dd356f8b
MG177 <mg.terok77@gmail.com>
2021-07-26 08:30:18 +0800

    idk

--- pages/room/createRoom.php
+++ pages/room/createRoom.php
@@ -40,49 +40,7 @@
 </head>
 
 <body>
-    <header>
-        <div class="left">
-            <img src="../../assets/img/Hero-logo.svg" alt="Logo" />
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-            </a>
-        </div>
-        <div class="wrap">
-            <div class="search">
-                <input type="text" class="searchTerm" placeholder="Search room" />
-                <button type="submit" class="searchButton">
-                    <i class="fa fa-search"></i>
-                </button>
-            </div>
-        </div>
-        <div class="right">
-            <button>Create Room</button>
-            <div class="user">
-                <?php
-              echo "
-              <p>".$_SESSION["nick"]."</p>
-              ";
-              ?>
-                <img src="../../assets/img/User-avatar.png" alt="User" />
-            </div>
-            <a href="../../assets/include/logout.inc.php">
-                <img src="../../assets/img/Exit.svg" alt="" />
-            </a>
-        </div>
-    </header>
-    <div class="dropdown" id="myLinks">
-        <ul>
-            <li><a href="#">Profile</a></li>
-            <hr>
-            <li><a href="#">Privacy</a></li>
-            <hr>
-            <li><a href="#">Menu</a></li>
-            <hr>
-            <li><a href="#">Menu</a></li>
-            <hr>
-            <li class="log-out-link"><a href="#">log out</a></li>
-        </ul>
-    </div>
+<?php include 'header.php'?>
 
     <div class="content-header">
         <ul>
