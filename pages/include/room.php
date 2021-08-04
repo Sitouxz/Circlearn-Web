4dc75170e1f4dd7acf0fb818f289a4402d396ddb
MG177 <mg.terok77@gmail.com>
2021-08-04 18:20:02 +0800

    history feature in progress

--- pages/include/room.php
+++ pages/include/room.php
@@ -3,7 +3,8 @@
             
             echo "
             <article>
-            <div class=\"img-container\">";if ($room['status'] == 1) {
+            <div class=\"img-container\">";
+            if ($room['status'] == 1) {
               echo "<img src='../assets/upload/banner".$room['roomId'].".jpg' class='banner2'/>";
             }else {
               echo "<img src='../assets/img/card-banner.png' class='banner2'/>";
