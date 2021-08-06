b69d8007a3b0bd7dfcc2a04c0e0fafd0f58957eb
Sitouxz <yehezkielowen0901@gmail.com>
2021-08-07 05:49:59 +0800

    profile fix and added login2

---
+++ pages/login2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login/Register | Circlearn</title>
+    <link rel="shortcut icon" href="../assets/img/Logo.svg" type="image/x-icon" />
+    <link rel="stylesheet" href="../assets/css/login2.css" />
+</head>
+
+<body>
+    <div class="login-container">
+        <div class="logo">
+            <img src="../assets/img/Logo.svg" alt="logo">
+        </div>
+        <div class="text">
+            <p>Rejoin the circle,
+                Enter the community
+                and learn together</p>
+            <h1>LOGIN</h1>
+        </div>
+        <div class="form">
+            <form action="">
+                <label>
+                    <span>Username/Email</span><br>
+                    <input type="text" name="email" />
+                </label>
+                <label>
+                    <span>Password</span><br>
+                    <input type="password" name="pwd" />
+                </label>
+                <button>LOGIN</button>
+            </form>
+        </div>
+        <div class="bottom-text">
+            <p>Don't have an account?</p>
+            <a href="#">Sign up</a>
+        </div>
+    </div>
+</body>
+
+</html>
